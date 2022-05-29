94a585a5ba3d0f875b5eb4c1d61ed42dfad33151
pradhyumanarora <pradhyumanarora@gmail.com>
2022-05-30 00:03:58 +0530

    Missed Navbar

--- project/frontend/src/components/Navbar/Navbar.js
+++ project/frontend/src/components/Navbar/Navbar.js
@@ -4,7 +4,7 @@ import './navbar.css'
 const Navbar = () =>{
     return (
         <div className="navbar navbar-green">
-            <h1 className="proj-title">Name of the Project</h1>
+            <h1 className="proj-title">EMOTION BASED MUSIC RECOMMENDATION SYSTEM</h1>
         </div>
     );
 };
